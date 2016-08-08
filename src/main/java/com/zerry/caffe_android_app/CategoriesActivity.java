8c0d5bb17791a92270132e6cbc1bd4bfabf3b435
Zheng Rui <rzhengphy@gmail.com>
2016-08-08 23:10:13 +0800

    add swiplist function

--- src/main/java/com/zerry/caffe_android_app/CategoriesActivity.java
+++ src/main/java/com/zerry/caffe_android_app/CategoriesActivity.java
@@ -1,8 +1,26 @@
 package com.zerry.caffe_android_app;
 
 import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baoyz.swipemenulistview.BaseSwipListAdapter;
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
+import java.util.List;
 
 /**
  * Created by zerry on 8/8/16.
@@ -10,6 +28,10 @@ import android.util.Log;
 public class CategoriesActivity extends Activity {
     private static final String TAG = "CategoriesActivity";
     private String task;
+    private List<ApplicationInfo> mAppList;
+    private AppAdapter mAdapter;
+    private SwipeMenuListView mListView;
+    private SwipeMenuCreator creator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +42,122 @@ public class CategoriesActivity extends Activity {
         Bundle extras = getIntent().getExtras();
         task = extras.getString("TASK");
         Log.i(TAG, task);
+
+        displayCategories(task);
+
     }
+
+    private int dp2px(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+                Resources.getSystem().getDisplayMetrics());
+    }
+
+    void displayCategories(String task) {
+        mAppList = getPackageManager().getInstalledApplications(0);
+        mListView = (SwipeMenuListView) findViewById(R.id.listView);
+        mAdapter = new AppAdapter();
+        mListView.setAdapter(mAdapter);
+
+        creator = new SwipeMenuCreator() {
+            @Override
+            public void create(SwipeMenu menu) {
+                SwipeMenuItem openItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                        0xCE)));
+                // set item width
+                openItem.setWidth(dp2px(90));
+                // set item title
+                openItem.setTitle("Reset");
+                // set item title fontsize
+                openItem.setTitleSize(18);
+                // set item title font color
+                openItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(openItem);
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(dp2px(90));
+                // set a icon
+                deleteItem.setIcon(R.mipmap.ic_delete);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+
+        mListView.setMenuCreator(creator);
+    }
+
+
+    class AppAdapter extends BaseSwipListAdapter {
+
+        @Override
+        public int getCount() {
+            return mAppList.size();
+        }
+
+        @Override
+        public ApplicationInfo getItem(int position) {
+            return mAppList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = View.inflate(getApplicationContext(),
+                        R.layout.item_list_app, null);
+                new ViewHolder(convertView);
+            }
+            ViewHolder holder = (ViewHolder) convertView.getTag();
+            ApplicationInfo item = getItem(position);
+            holder.iv_icon.setImageDrawable(item.loadIcon(getPackageManager()));
+            holder.tv_name.setText(item.loadLabel(getPackageManager()));
+            holder.iv_icon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(CategoriesActivity.this, "iv_icon_click", Toast.LENGTH_SHORT).show();
+                }
+            });
+            holder.tv_name.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(CategoriesActivity.this,"iv_icon_click",Toast.LENGTH_SHORT).show();
+                }
+            });
+            return convertView;
+        }
+
+        class ViewHolder {
+            ImageView iv_icon;
+            TextView tv_name;
+
+            public ViewHolder(View view) {
+                iv_icon = (ImageView) view.findViewById(R.id.iv_icon);
+                tv_name = (TextView) view.findViewById(R.id.tv_name);
+                view.setTag(this);
+            }
+        }
+
+        @Override
+        public boolean getSwipEnableByPosition(int position) {
+            if(position % 2 == 0){
+                return false;
+            }
+            return true;
+        }
+    }
+
+
 }
