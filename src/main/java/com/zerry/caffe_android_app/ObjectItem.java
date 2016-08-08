c83b7a2d44b9eafe4a5e9e3828e2263b7cbbe69c
Zheng Rui <rzhengphy@gmail.com>
2016-08-09 06:35:47 +0800

    place each package in subfolder

---
+++ src/main/java/com/zerry/caffe_android_app/ObjectItem.java
@@ -0,0 +1,18 @@
+package com.zerry.caffe_android_app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zerry on 16/8/9.
+ */
+public class ObjectItem {
+    public String thumbnailPth;
+    public int featureLength;
+    public int featureNum;
+    public List<float[]> features = new ArrayList<>();
+
+    public ObjectItem(String pth) {
+        this.thumbnailPth = pth;
+    }
+}
